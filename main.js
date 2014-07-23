dd495fb2eb45c957958efd6faf3f104d71e982e6
Bryce Crawford <brycecrawford1@gmail.com>
2014-07-23 10:48:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ function createMap(currentPosition) {
 
 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	
+	var marker = new google.maps.Marker({
+      	position: myLatlng,
+      	map: map,
+      	title: 'Your location'
+  	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
