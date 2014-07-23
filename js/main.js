eee30cdc704cc64af103efd0d1300cffbd25e061
Scott Walters <me@scowalt.com>
2014-07-23 14:51:26 -0500

    Added types of businesses as JS vars

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 var map = null; //makes map global
 
+var HOTELS = ["Hilton",
+	"Marriott",
+	"Omni Hotels",
+	"Wyndham Hotels",
+	"Days Inn",
+	"Super 8",
+	"Trevelodge",
+	"Holiday Inn",
+	"Motel 6",
+	"Westin",
+	"Best Western"
+];
+var DEPARTMENT_STORES = ["Sears",
+	"Lowe’s",
+	"The Home Depot",
+	"Macy’s",
+	"Walmart",
+	"Bath and Body Works",
+	"Kmart",
+	"Office Depot"
+];
+var OFFICE_SUPPLIES = [
+	"Best Buy",
+	"Staples",
+	"Office Depot",
+	"Office Max",
+	"Apple"
+
+]
+
 function initialize() {
 	navigator.geolocation.getCurrentPosition(createMap);
 }
@@ -37,21 +67,26 @@ function start() {
 function searchStores(currentPosition) {
 	var pos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
 
-	var searchCriteria = prompt("Search stores in your area")
+	//var searchCriteria = prompt("Search stores in your area")
+	var type = HOTELS;
+	for (var i = 0; i < type.length; i++) {
+		var name = type[i];
 
-	var request = {
-		location: pos,
-		radius: 2000,
-		types: [searchCriteria]
-	};
+		var request = {
+			location: pos,
+			radius: 10000,
+			name: name
+		};
 
-	if (map != null) {
-		var service = new google.maps.places.PlacesService(map);
-		service.nearbySearch(request, find);
+		if (map != null) {
+			var service = new google.maps.places.PlacesService(map);
+			service.nearbySearch(request, find);
+		}
 	}
 }
 
 function find(results, status) {
+	console.log(status);
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
 		for (var i = 0; i < results.length; i++) {
 			createMarker(results[i]);
