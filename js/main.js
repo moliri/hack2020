60df92cd88d4a4666ac3034318635bbfdac684f3
Bryce Crawford <brycecrawford1@gmail.com>
2014-07-23 13:52:44 -0500

    Added search for store ability and made map global

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var map = null; //makes map global
+
 function initialize() {
 	navigator.geolocation.getCurrentPosition(createMap);
 }
@@ -12,7 +14,7 @@ function createMap(currentPosition) {
 
 	var mapCanvas = $("#map-canvas")[0];
 	console.log(mapCanvas);
-	var map = new google.maps.Map(mapCanvas, mapOptions);
+	map = new google.maps.Map(mapCanvas, mapOptions);
 	console.log("test");
 	
 	var marker = new google.maps.Marker({
@@ -31,6 +33,49 @@ $(document).on('pageinit', '#intropage', function(){
 function start() {
 	$.mobile.changePage('#maps');  
 	return false;
+}
+
+function searchStores(currentPosition) {
+	var pos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
+	
+	var searchCriteria = prompt("Search stores in your area")
+
+	var request = {
+		location: pos,
+		radius: 2000,
+		types: [searchCriteria]
+	};
+	
+	if(map != null) {
+		var service = new google.maps.places.PlacesService();
+	}
+	
+	service.nearbySearch(request, find);
+}
+
+function find(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+	map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+	if(map != null) {
+		infowindow.open(map, this);
+	}
+  });
+  
+  console.log("created marker");
 }
 	
 
