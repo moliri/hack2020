eec9ca71ed772611deccc363d19907f268b9f84c
moliri <leesha.maliakal@gmail.com>
2014-07-23 13:18:06 -0500

    adding UI content to dev branch

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function initialize() {
+	navigator.geolocation.getCurrentPosition(createMap);
+}
+
+function createMap(currentPosition) {
+	var pos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
+
+	var mapOptions = {
+		center: pos,
+		zoom: 14
+	};
+
+	var mapCanvas = $("map-canvas");
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+	console.log("test");
+	
+	var marker = new google.maps.Marker({
+      	position: pos,
+      	map: map,
+      	title: 'Your Location'
+  	});
+	
+}
+
+/* starting script for intro page */
+$(document).on('pageinit', '#intropage', function(){
+	$('#startBtn').click(start);
+});
+
+function start() {
+	$.mobile.changePage('#maps');  
+	return false;
+}
+	
+
+google.maps.event.addDomListener(window, 'load', initialize);
