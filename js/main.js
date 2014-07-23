7920bb1a8c56a1258a678c6719ae680cd9d85c6f
Scott Walters <me@scowalt.com>
2014-07-23 14:31:05 -0500

    Fixed markers not displaying

--- js/main.js
+++ js/main.js
@@ -14,29 +14,29 @@ function createMap(currentPosition) {
 
 	var mapCanvas = $("#map-canvas")[0];
 	map = new google.maps.Map(mapCanvas, mapOptions);
-	
+
 	var marker = new google.maps.Marker({
-      	position: pos,
-      	map: map,
-      	title: 'Your Location'
-  	});
-	
+		position: pos,
+		map: map,
+		title: 'Your Location'
+	});
+
 	searchStores(currentPosition);
 }
 
 /* starting script for intro page */
-$(document).on('pageinit', '#intropage', function(){
+$(document).on('pageinit', '#intropage', function() {
 	$('#startBtn').click(start);
 });
 
 function start() {
-	$.mobile.changePage('#maps');  
+	$.mobile.changePage('#maps');
 	return false;
 }
 
 function searchStores(currentPosition) {
 	var pos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
-	
+
 	var searchCriteria = prompt("Search stores in your area")
 
 	var request = {
@@ -44,37 +44,27 @@ function searchStores(currentPosition) {
 		radius: 2000,
 		types: [searchCriteria]
 	};
-	
-	if(map != null) {
+
+	if (map != null) {
 		var service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(request, find);
 	}
-	
-	service.nearbySearch(request, find);
 }
 
 function find(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+			createMarker(results[i]);
+		}
+	}
 }
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-	map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-	if(map != null) {
-		infowindow.open(map, this);
-	}
-  });
-  
-  console.log("created marker");
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
 }
 
 
